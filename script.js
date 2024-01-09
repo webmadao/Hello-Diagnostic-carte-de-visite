d6ec80646e3b136c44e9266616a570ea37d75f94
Ilyes Kadji <web.kin.dev@gmail.com>
2024-01-09 15:33:37 +0100

    partage reseau sociaux

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 const modal = document.querySelector("#modal");
 const btnOpenModal = document.querySelector("#openmodal");
 const btnCloseModal = document.querySelector("#closemodal");
-const btnWhatsapp = document.querySelector(".whatsapp");
-const btnMail = document.querySelector(".mail");
+
 
 btnOpenModal.addEventListener("click", () => {
     modal.style.display = "block";
@@ -13,15 +12,3 @@ btnCloseModal.addEventListener("click", () => {
     modal.classList.remove("animate__animated", "animate__fadeIn");
 });
 
-btnWhatsapp.addEventListener("click", () => {
-    const currentUrl = encodeURIComponent(window.location.href);
-    const whatsappUrl = `https://wa.me/5491154245916?text=Hola%20estoy%20interesado%20en%20tu%20servicio%20&url=${currentUrl}`;
-    window.open(whatsappUrl);
-});
-
-
-btnMail.addEventListener("click", () => {
-    const currentUrl = encodeURIComponent(window.location.href);
-    const mailtoUrl = `mailto:KQ5k6@example.com?subject=Hola%20estoy%20interesado%20en%20tu%20servicio%20&body=${currentUrl}`;
-    window.open(mailtoUrl);
-});
