bf3c8580fcbaf3841f77ab00da0817babcba4c6d
Ilyes Kadji <web.kin.dev@gmail.com>
2024-01-09 15:08:22 +0100

    modal

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-const btnWhatsapp = document.querySelector(".whatsapp")
-const btnmail = document.querySelector(".mail");
+const modal = document.querySelector("#modal");
+const btnOpenModal = document.querySelector("#openmodal");
+const btnCloseModal = document.querySelector("#closemodal");
+const btnWhatsapp = document.querySelector(".whatsapp");
+const btnMail = document.querySelector(".mail");
+
+btnOpenModal.addEventListener("click", () => {
+    modal.style.display = "block";
+});
+
+btnCloseModal.addEventListener("click", () => {
+    modal.style.display = "none";
+    modal.classList.remove("animate__animated", "animate__fadeIn");
+});
 
 btnWhatsapp.addEventListener("click", () => {
     const currentUrl = encodeURIComponent(window.location.href);
@@ -8,7 +20,7 @@ btnWhatsapp.addEventListener("click", () => {
 });
 
 
-btnmail.addEventListener("click", () => {
+btnMail.addEventListener("click", () => {
     const currentUrl = encodeURIComponent(window.location.href);
     const mailtoUrl = `mailto:KQ5k6@example.com?subject=Hola%20estoy%20interesado%20en%20tu%20servicio%20&body=${currentUrl}`;
     window.open(mailtoUrl);
